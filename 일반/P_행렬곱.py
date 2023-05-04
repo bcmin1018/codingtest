d2b0310d55476dc9784d57acd5eac7cf4f9877c7
민병철 <bcmin@funnywork.co.kr>
2023-05-04 13:56:16 +0900

    행렬곱 차2

--- 일반/P_행렬곱.py
+++ 일반/P_행렬곱.py
@@ -1,30 +1,20 @@
 def solution(arr1, arr2):
-    n = len(arr1[0])
-    m = len(arr1)
+    n = len(arr1)
+    m = len(arr2)
+    k = len(arr2[0])
+
     answer = [[0 for i in range(n)] for j in range(m)]
 
     for i in range(n):
-        for lst1 in arr1:
-            nlist2 = []
-            for lst2 in arr2:
-                nlist2.append(lst2[i])
-            print(lst1 * nlist2)
-
-
-
-
-
-    # answer = [[0 for m in range(arr2)] for i in range(arr1)]
-    # for i, node1 in enumerate(arr1):
-    #     for di in range(n):
-    #         sum_ = 0
-    #         for j, node2 in enumerate(arr2):
-    #             sum_ += node1[di] * node2[di]
-    #         answer[i][j] = sum_
+        for j in range(m):
+            sum_ = 0
+            for k in range(k):
+                sum_ += arr1[i][k] * arr2[k][j]
+            answer[i][j] = sum_
     return answer
 
-arr1 = [[2, 3, 2], [4, 2, 4], [3, 1, 4]]
-arr2 = [[5, 4, 3], [2, 4, 1], [3, 1, 1]]
+arr1 = [[1,2,3,4], [1,2,3,4]]
+arr2 =  [[1,2], [1,2], [1,2], [1,2]]
 
 print(solution(arr1, arr2))
 
