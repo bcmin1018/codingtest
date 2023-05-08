673cd194a968e6a8481bb49f5f9aa97e319e5a7d
민병철 <bcmin@funnywork.co.kr>
2023-05-08 10:49:45 +0900

    행렬곱 성공

--- 일반/P_행렬곱.py
+++ 일반/P_행렬곱.py
@@ -1,20 +1,23 @@
 def solution(arr1, arr2):
     n = len(arr1)
-    m = len(arr2)
-    k = len(arr2[0])
+    m = len(arr2[0])
+    k = len(arr1[0]) # 2*4  4*2  겹치는 것은 4
 
-    answer = [[0 for i in range(n)] for j in range(m)]
+    answer = [[0 for _ in range(m)] for _ in range(n)]
 
     for i in range(n):
         for j in range(m):
             sum_ = 0
-            for k in range(k):
-                sum_ += arr1[i][k] * arr2[k][j]
+            for ki in range(k):
+                sum_ += arr1[i][ki] * arr2[ki][j]
             answer[i][j] = sum_
     return answer
 
-arr1 = [[1,2,3,4], [1,2,3,4]]
-arr2 =  [[1,2], [1,2], [1,2], [1,2]]
+# arr1 = [[1, 4], [3, 2], [4, 1]]
+# arr2 = [[3, 3], [3, 3]]
+
+arr1 = [[2, 3, 2], [4, 2, 4], [3, 1, 4]]
+arr2 = [[5, 4, 3], [2, 4, 1], [3, 1, 1]]
 
 print(solution(arr1, arr2))
 
