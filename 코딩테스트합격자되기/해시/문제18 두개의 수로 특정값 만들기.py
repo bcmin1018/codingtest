ecd345ee5cf9ffd0c527c2a54583349f6a7f1b93
byeongcheolmin <bcmin1018@gmail.com>
2024-02-04 10:46:20 +0900

    해시

--- 코딩테스트합격자되기/해시/문제18 두개의 수로 특정값 만들기.py
+++ 코딩테스트합격자되기/해시/문제18 두개의 수로 특정값 만들기.py
@@ -5,17 +5,28 @@ def count_sort(arr, k):
             hash_table[num] = 1
     return hash_table
 
-def solution(arr, target):
-    hash_table = count_sort(arr, target)
+def create_hash_table(arr):
+    hash_table = [0] * (max(arr) + 1)
+    for num in arr:
+        hash_table[num] = 1
+    return hash_table
 
+
+def solution(arr, target):
+    # hash_table = count_sort(arr, target)
+    answer = False
+    hash_table = create_hash_table(arr)
     for num in arr:
         key = target - num
-        if hash_table[key] == 0:
-            continue
-        else:
+        if hash_table[key] == 1 and key != num:
+            answer = True
+
+    return answer
 
 
 arr = [1,2,3,4,8]
 target = 6
 
-solution(arr, target)
+# arr = [2,3,5,9]
+# target = 10
+print(solution(arr, target))
