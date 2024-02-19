702bca9405dc1bc73796fd825d8033639fbdb704
byeongcheolmin <bcmin1018@gmail.com>
2024-02-19 23:10:54 +0900

    그리디 몸풀기

---
+++ 코딩테스트합격자되기/그리디/문제80 거스름돈 주기.py
@@ -0,0 +1,16 @@
+def solution(amount):
+    moneny = [1, 10, 50, 100]
+    result = []
+
+    for i, m in enumerate(reversed(moneny)):
+        while True:
+            if amount < m:
+                break
+            else:
+                amount = amount - m
+                result.append(m)
+
+    return result
+
+amount = 350
+print(solution(amount))
