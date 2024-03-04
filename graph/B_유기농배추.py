2ff35c739c2aeb8cb524cca11944f7fd85aa7bbe
byeongcheolmin <bcmin1018@gmail.com>
2024-03-05 07:54:42 +0900

    그래프

---
+++ graph/B_유기농배추.py
@@ -0,0 +1,17 @@
+T = int(input())
+M, N, K = map(int, input().split())
+
+bat = list([0] * M for _ in range(N))
+print(bat)
+
+# baechu = []
+# for _ in K:
+#     baechu.append(list(map(int, input().split())))
+#
+# print(baechu)
+#
+# for i in range(M):
+#     for j in range(N):
+
+
+
