a84e4a4f8c6c69ae944c164dc0157d0f164dc9c1
bcmin <bcmin1018@gmail.com>
2024-03-19 10:12:14 +0900

    기능 개발 완료

--- 코딩테스트합격자되기/큐/문제16 기능개발.py
+++ 코딩테스트합격자되기/큐/문제16 기능개발.py
@@ -1,16 +1,12 @@
 import math
 
-
 def solution(progresses, speeds):
     days = [math.ceil((100 - progresses[i]) / speeds[i]) for i in range(0, len(progresses))]
     answer = []
-    days.append(1000)
-
-    cnt = 1
-    for i in range(0, len(days) - 1):
-        if days[i + 1] <= days[i]:
-            cnt += 1
-        else:
-            answer.append(cnt)
-            cnt = 1
+    now = 0
+    for i in range(1, len(days)):
+        if days[i] > days[now]:
+            answer.append(i-now)
+            now = i
+    answer.append(len(days)-now)
     return answer
