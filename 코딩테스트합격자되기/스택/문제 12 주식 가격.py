e5f759d8fa89430054c1a4192c1726b13dbf624a
bcmin <bcmin1018@gmail.com>
2024-03-21 10:27:32 +0900

    스택

--- 코딩테스트합격자되기/스택/문제 12 주식 가격.py
+++ 코딩테스트합격자되기/스택/문제 12 주식 가격.py
@@ -1,1 +1,27 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/42584
+
+# def solution(prices):
+#     answer = []
+#     for i in range(0, len(prices)):
+#         cnt = 0
+#         for j in range(i+1, len(prices)):
+#             if prices[j] < prices[i]:
+#                 cnt+=1
+#                 break
+#             cnt+=1
+#         answer.append(cnt)
+#     return answer
+
+def solution(prices):
+    answer = []
+    stack = []
+    for i in range(0, len(prices)):
+        while stack and prices[stack[-1]] > prices[i]:
+            j = stack.pop()
+            answer.append(i - j)
+        stack.append(i)
+
+    while stack:
+        j = stack.pop()
+        answer.append(len(prices) - j)
+    return answer
