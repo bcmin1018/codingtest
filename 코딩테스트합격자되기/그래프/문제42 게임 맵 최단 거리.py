7b874d1b73f88393d110ff5d95b20700d7814e80
Brad.Min <bcmin1018@gmail.com>
2024-03-23 13:21:50 +0900

    BFS 복습

--- 코딩테스트합격자되기/그래프/문제42 게임 맵 최단 거리.py
+++ 코딩테스트합격자되기/그래프/문제42 게임 맵 최단 거리.py
@@ -1,32 +1,39 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/1844
+# BFS를 사용하여 풀기
+# 큐는 큐에 넣을때 방문을 표시한다.
+
 from collections import deque
+
 def solution(maps):
-    direction = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-    n = len(maps) #세로
-    m = len(maps[0]) #가로
+    x = len(maps[0])
+    y = len(maps)
 
-    dist = [[-1] * m for _ in range(n)]
-
+    visited = [[0] * x for _ in range(0, y)]
+    queue = deque()
     def bfs(start):
-        queue = deque([start])
-        dist[start[0]][start[1]] = 1
+        visited[start[1]][start[0]] = 1
+        queue.append(start)
+
+        if start[1] == y and start[0] == x:
+            return
 
         while queue:
-            x, y = queue.popleft()
-            for d in direction:
-                nx, ny = x + d[0], y + d[1]
-                if nx < 0 or nx >= m or ny < 0 or ny >= n:
-                    continue
-
-                if maps[ny][nx] ==0:
-                    continue
-
-                if dist[ny][nx] == -1:
-                    queue.append([nx, ny])
-                    dist[ny][nx] = dist[y][x] + 1
-        return dist
-
-    bfs([0,0])
-    return dist[n-1][m-1]
+            x_, y_ = queue.popleft()
+            for i, j  in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+                nx, ny = x_+i, y_+j
+                if 0<= nx < x and 0 <= ny < y and maps[ny][nx] != 0 and visited[ny][nx] == 0:
+                    queue.append((nx, ny))
+                    visited[ny][nx] = visited[y_][x_] + 1
 
 
+    bfs((0,0))
 
+    if visited[y-1][x-1] == 0:
+        return -1
+    else:
+        return visited[y-1][x-1]
+
+
+# maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]
+maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]
+print(solution(maps))
