bfd2fd3e69eca61dfb2e6fad29bfb3a3b8a7fcae
bcmin <bcmin1018@gmail.com>
2024-03-29 10:06:22 +0900

    DFS 피로도 성공

--- DFS&BFS/P_피로도.py
+++ DFS&BFS/P_피로도.py
@@ -1,24 +1,18 @@
+answer = 0
 def solution(k, dungeons):
     visited = [0] * len(dungeons)
-    def dfs(node, k):
-        print(k)
-        if dungeons[node][0] > k: # 최소 피로도 보다 피가 낮으면 다른 곳 찾아!
-            return
-        k = k - dungeons[node][1] # 던전 깨고 피로도 차감
-        visited[node] = 1
-        for i in [1, -1]: # 다음 곳 서치
-            n_node = node+i
-            if n_node < len(dungeons) and visited[n_node] == 0 and dungeons[n_node][0] <= k:
-                print(f'{dungeons[n_node]}')
-                dfs(n_node, k)
-                # print(n_node)
-                # print(node, n_node)
-                # print(visited)
-                # visited[n_node] = visited[node] + 1
 
+    def dfs(k, dungeons, cnt):
+        global answer
+        answer = max(answer, cnt)
+        for i in range(len(dungeons)):
+            if visited[i] == 0 and dungeons[i][0] <= k:
+                visited[i] = 1
+                dfs(k-dungeons[i][1], dungeons, cnt+1)
+                visited[i] = 0
 
-    dfs(0, k)
-    return sum(visited)
+    dfs(k, dungeons, 0)
+    return answer
 
 
 
