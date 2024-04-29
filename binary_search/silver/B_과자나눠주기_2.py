ebcc4a46c78be5d21dfa355034e87f684eb0500d
bcmin <bcmin1018@gmail.com>
2024-04-29 09:39:23 +0900

    과자나눠주기 복습

--- binary_search/silver/B_과자나눠주기_2.py
+++ binary_search/silver/B_과자나눠주기_2.py
@@ -1,12 +1,12 @@
+# 최대 막대과자 길이를 구해야하므로 upper bound로 구해야함.
 M, N = map(int, input().split())
 cookies = list(map(int, input().split()))
-left, right = 1, max(cookies)
+left, right = 1, len(cookies)
 while left <= right:
     mid = (left+right) // 2
-    max_person = sum([c//mid for c in cookies])
-    if max_person >= M: # 작게 나눈 경우
-        left = mid+1
+    total = sum([c//mid for c in cookies])
+    if total >= M:
+        left = mid + 1
     else:
-        right = mid-1
-print(right)
-
+        right = mid - 1
+print(left-1)
