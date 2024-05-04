dca3321f73699ad339fda490fbf2c4a1141946a2
Brad.Min <bcmin1018@gmail.com>
2024-05-04 11:02:39 +0900

    시간초과

--- Priority_Queue/silver/B_맥주죽체_1(시간초과).py
+++ Priority_Queue/silver/B_맥주죽체_1(시간초과).py
@@ -1,31 +1,52 @@
 # https://www.acmicpc.net/problem/17503
 # 우선순위 큐
 import heapq
-N, M, K = map(int, input().split())
+N, M, K = map(int, input().split()) #N: 열리는 기간, M: 선호도 합 K: 맥주 종류의 수
 beers = []
+max_ = 0
 for _ in range(0, K):
-    beers.append(list(map(int, input().split())))
+    beer = list(map(int, input().split()))
+    if beer[1] > max_:
+        max_ = beer[1]
+    beers.append(beer)
 
-# 최소의 도수를 찾아야하므로 도수로 정렬
-beers = sorted(beers, key=lambda x: (x[1], x[0]))
+# 최소의 도수를 찾아야하므로 도수로 오름차순 정렬
+# beers = sorted(beers, key=lambda x: (x[1], x[0]))
+#
+# like_sum = 0
+# answer = 0
+# queue = []
+# for b in beers:
+#     like_sum += b[0]
+#     answer = max(answer, b[1])
+#     heapq.heappush(queue, b[0])
+#     if len(queue) == N:
+#         if like_sum >= M:
+#             print(answer)
+#             break
+#         else:
+#             min_like = heapq.heappop(queue)
+#             like_sum -= min_like
+# else:
+#     print(-1)
 
-like_sum = 0
-answer = 0
-queue = []
-for b in beers:
-    like_sum += b[0]
-    answer = max(answer, b[1])
-    heapq.heappush(queue, b[0])
-    if len(queue) == N:
-        if like_sum >= M:
-            print(answer)
-            break
-        else:
-            min_like = heapq.heappop(queue)
-            like_sum -= min_like
-else:
-    print(-1)
+
+# 시간 초과가 계속 발생하는데 어떻게 풀까... 조건에 맞는 것 중에 도수의 최소값을 구하는 방식 -> 이분 탐색 lower bound
+# 이렇게 변경했는데 계속 시간 초과.. 뭐가 문제일까
+beers = sorted(beers, key=lambda x: x[0])
+left, right = 0, max_
+result = -1
+while left <= right:
+    mid = (left+right) // 2
+    likes = [beer[0] for beer in beers if beer[1] <= mid]
+    total = sum(likes[-N:])
+    if total >= M:
+        right = mid - 1
+        result = right + 1
+    else:
+        left = mid + 1
+print(result)
 
 
-# 시간 초과가 계속 발생하는데 어떻게 풀까...
+
 
--- binary_search/silver/B_나무자르기_2.py
+++ binary_search/silver/B_나무자르기_2.py
@@ -1,3 +1,4 @@
+# https://www.acmicpc.net/problem/2805
 # 높이의 최대값 즉 같은 높이를 가져갈 수 있어도 최대한 나무를 회손 시키지 않기 위해 절단기를 최대한 높이 설정해야한다.
 # upperbound로 구하고 right 값을 출력하면 가져갈 수 있는 수 중에 최대 절단 높이를 구할 수 있다.
 # https://www.acmicpc.net/problem/2805
@@ -23,10 +24,10 @@ left, right = 1, max(trees)
 while left <= right:
     mid = (left + right) // 2
     total = sum([t - mid for t in trees if t > mid])
-    if total >= M:
-        left = mid + 1
-    else:
+    if total >= M: # total이 기준보다 크면 최솟값을 차자야하기 때문에 작은 쪽으로 유도한다.
         right = mid - 1
+    else:
+        left = mid + 1
 print(left -1)
 
 
