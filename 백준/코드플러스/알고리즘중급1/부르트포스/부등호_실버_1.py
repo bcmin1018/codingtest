4bcbba30b0f4cc77de27c24269b47b2188137b7f
bcmin <bcmin1018@gmail.com>
2024-05-28 11:13:39 +0900

    부등호 실버

--- 백준/코드플러스/알고리즘중급1/부르트포스/부등호_실버_1.py
+++ 백준/코드플러스/알고리즘중급1/부르트포스/부등호_실버_1.py
@@ -7,7 +7,7 @@ sign = list(input().split())
 num = [i for i in range(0, 10)]
 answer = []
 for case in permutations(num, n+1):
-    for i in range(0, n+1):
+    for i in range(0, n):
         if sign[i] == ">":
             if case[i] < case[i+1]:
                 break
@@ -16,7 +16,10 @@ for case in permutations(num, n+1):
                 break
     else:
         answer.append(case)
-print(answer)
+answer.sort()
+print("".join(map(str, answer[-1])))
+print("".join(map(str, answer[0])))
+
 
 
 
