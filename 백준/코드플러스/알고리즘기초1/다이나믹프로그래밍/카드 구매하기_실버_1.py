59b08e369df110c618447113db6f055fe5ea8592
Brad.Min <bcmin1018@gmail.com>
2024-06-02 23:57:04 +0900

    ㅋㅏ드 구매하기

--- 백준/코드플러스/알고리즘기초1/다이나믹프로그래밍/카드 구매하기_실버_1.py
+++ 백준/코드플러스/알고리즘기초1/다이나믹프로그래밍/카드 구매하기_실버_1.py
@@ -1,4 +1,11 @@
-# https://xn--acmicpc-wk2a.net/problem/11052
+# https://www.acmicpc.net/problem/11052
 
-n = int(input())
+N = int(input())
+lst = [0] + list(map(int, input().split()))
+dp = [0] * (N + 1)
 
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        if j - i >= 9:
+            dp[j] = max(dp[j], dp[j - i] + lst[i])
+        print(dp[N])
