c753b8126ef175a1c760d5cc20fa2da5842532da
Brad.Min <bcmin1018@gmail.com>
2024-06-16 23:43:08 +0900

    줄세우기 골드

---
+++ 백준/코드플러스/코딩 테스트 문제/그래프/줄세우기_골드_3.py
@@ -0,0 +1,32 @@
+# https://www.acmicpc.net/problem/2252
+# 위상 정렬이라는 것을 배웠다. 싸이클이 없는 방향 그래프인 경우 위상 정렬을 사용하자.
+
+from collections import deque
+
+N, M = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+indegree = [0] * (N+1)
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b] += 1
+
+result = []
+q = deque()
+for i in range(1, N+1):
+    if indegree[i] == 0:
+        q.append(i)
+
+while q:
+    now = q.popleft()
+    result.append(now)
+
+    for i in graph[now]:
+        indegree[i] -= 1
+
+        if indegree[i] == 0:
+            q.append(i)
+
+for i in result:
+    print(i, end=" ")
