5df67a7fe1cd07c9fc80f7510ca6f797023156d5
bcmin <bcmin1018@gmail.com>
2024-06-25 10:01:17 +0900

    가장 긴 증가하는 부분수열

--- 백준/코드플러스/알고리즘기초1/다이나믹프로그래밍/가장긴증가하는부분수열_실버_2.py
+++ 백준/코드플러스/알고리즘기초1/다이나믹프로그래밍/가장긴증가하는부분수열_실버_2.py
@@ -3,10 +3,11 @@ N = int(input())
 lst = [0] + list(map(int, input().split()))
 dp = [0] * (N+1)
 
-for i in range(1, N):
-    if lst[i] < lst[i+1]:
-        dp[i] = dp[i-1] + 1
-    else:
-        dp[i] = dp[i-1]
+for i in range(1, N+1):
+    mx = 0
+    for j in range(0, i): # 이전까지의 숫자를 모두 순회 하면서 현재보다 큰게 있는지 확인한다.
+        if lst[i] > lst[j]:
+            mx = max(mx, dp[j]) #
+    dp[i] = mx + 1 # 이전까지 숫자 중에서 가장 큰 값보다 + 1
 
-print(max(dp)+1)
+print(max(dp))
