ccb6e57286e2fdba49d422373045195d7fd149ed
Brad.Min <bcmin1018@gmail.com>
2024-07-03 23:52:25 +0900

    벽부수고 이동하기 수정

--- 백준/코드플러스/알고리즘중급1/BFS/벽부수고이동하기_골드_2.py
+++ 백준/코드플러스/알고리즘중급1/BFS/벽부수고이동하기_골드_2.py
@@ -3,46 +3,28 @@ from collections import deque
 
 N, M = map(int, input().split())
 maps = [list(map(int, input())) for _ in range(N)]
+v = [[[0, 0] for _ in range(M) for _ in range(N)]]
 
-walls = []
-for y in range(N):
-    for x in range(M):
-        if maps[y][x] == 1:
-            walls.append((x, y))
+def bfs(x, y):
 
-def bfs(node):
-    v = [[0] * M for _ in range(N)]
     q = deque()
-    q.append(node)
-    v[node[1]][node[0]] = 1
+    q.append((x, y, 0))
+    v[y][x][0] = 1
 
     while q:
-        x, y = q.popleft()
+        x, y, z = q.popleft()
         if x == M - 1 and y == N - 1:
-            return v[N-1][M-1]
+            return v[N-1][M-1][z]
 
         for i, j in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
             nx, ny = x + i, y + j
-            if 0 <= nx < M and 0 <= ny < N and v[ny][nx] == 0 and maps[ny][nx] == 0:
-                q.append((nx, ny))
-                v[ny][nx] = v[y][x] + 1
+            if 0 <= nx < M and 0 <= ny < N:
+                if maps[ny][nx] == 1 and z == 0:
+                    v[ny][nx][1] = v[y][x][0] + 1
+                if maps[ny][nx] == 0 and v[ny][nx][z] == 0:
+                    v[ny][nx][z] = v[y][x][z] + 1
+                    q.append((nx, ny, z))
 
-answer = []
-if len(walls) > 0:
-    for w in walls:
-        maps[w[1]][w[0]] = 0
-        d = bfs((0, 0))
-        maps[w[1]][w[0]] = 1
-        if d == None:
-            continue
-        else:
-            answer.append(d)
-else:
-    d = bfs((0, 0))
-    answer.append(d)
+    return -1
 
-
-if len(answer) == 0:
-    print(-1)
-else:
-    print(min(answer))
+print(bfs(0, 0))
