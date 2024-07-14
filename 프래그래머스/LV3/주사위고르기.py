de8995eec4708519889d9e15614afc43b1e0b8f6
Brad.Min <bcmin1018@gmail.com>
2024-07-14 23:48:37 +0900

    주사위고르기

--- 프래그래머스/LV3/주사위고르기.py
+++ 프래그래머스/LV3/주사위고르기.py
@@ -1,2 +1,19 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/258709?language=python3
+from itertools import combinations
 
+def dfs(n, dice1, dice2, exit):
+    if n == exit:
+        return
+
+
+def solution(dice):
+    n = len(dice)
+    cases = list(combinations(dice, n//2))
+    for case in cases:
+        dfs(n, )
+    answer = []
+    return answer
+
+dice = [[1, 2, 3, 4, 5, 6], [3, 3, 3, 3, 4, 4], [1, 3, 3, 4, 4, 4], [1, 1, 4, 4, 5, 5]]
+
+solution(dice)
