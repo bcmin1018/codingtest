8ccd6838b72e295e3fe381b811d4f71356fe7838
Brad.Min <bcmin1018@gmail.com>
2024-08-21 23:36:31 +0900

    리코쳇로봇 완료

--- 프래그래머스/LV2/리코쳇로봇_BFS.py
+++ 프래그래머스/LV2/리코쳇로봇_BFS.py
@@ -4,35 +4,45 @@
 from collections import deque
 
 def solution(board):
-    answer = 0
     x, y = len(board[0]), len(board),
     board = [list(b) for b in board]
     v = [[0] * x for _ in range(y)]
-    print(board)
-    # print(v)
+
     def bfs(node):
+        answer = 0
         q = deque()
         v[node[0]][node[1]] = 1
         q.append(node)
         while q:
-            i, j = q.popleft()
+            i, j, level = q.popleft()
             if board[i][j] == 'G':
-                return
+                return level
 
             for ii, jj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                ni, nj = i + ii, j + jj
-                if 0<=ni<y and 0<=nj<x and board[ni][nj] != 'D  ' and v[ni][nj] == 0:
-                    v[ni][nj] = v[i][j] + 1
-                    q.append((ni, nj))
+                now_i, now_j = i, j
+                while True:
+                    next_i, next_j = now_i+ii, now_j+jj
+                    # 장애물이나 맨끝에 도착하면 스탑
+                    # 끝까지 가면 방문 표시한다.
+                    # 끝까지 간 좌표를 큐에 넣는다.
+                    if 0 <= next_i < y and 0 <= next_j < x and board[next_i][next_j] != 'D':
+                        now_i, now_j = next_i, next_j
+                        continue
+                    break
+                if 0 <= now_i < y and 0 <= now_j < x and v[now_i][now_j] == 0:
+                    v[now_i][now_j] = 1
+                    q.append((now_i, now_j, level+1))
 
-
+    answer = 0
     for i in range(len(board)):
         for j in range(len(board[0])):
             if board[i][j] == 'R':
-                bfs((i, j))
-    print(v)
+                answer = bfs((i, j, 0))
+    if answer is None:
+        return -1
     return answer
 
 
-test = ["...D..R", ".D.G...", "....D.D", "D....D.", "..D...."]
-solution(test)
+# test = ["...D..R", ".D.G...", "....D.D", "D....D.", "..D...."]
+test = [".D.R", "....", ".G..", "...D"]
+print(solution(test))
