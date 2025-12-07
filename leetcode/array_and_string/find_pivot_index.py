b486aa89d4fcb3e7a94db52322f221ac73f01ea0
Brad.Min <bcmin1018@gmail.com>
2025-12-07 23:28:08 +0900

    시간 복잡도 추가

--- leetcode/array_and_string/find_pivot_index.py
+++ leetcode/array_and_string/find_pivot_index.py
@@ -1,3 +1,17 @@
+"""
+시간 복잡도: O(n²)
+- 외부 루프: O(n) (n = nums.length, 1 <= n <= 10^4)
+- 내부 sum 연산: 반복마다 O(n) (최악의 경우)
+- 총 시간 복잡도: O(n²) = 최악의 경우 O(10^8)
+
+공간 복잡도: O(1)
+- 상수 공간만 사용
+
+제약 조건:
+- 1 <= nums.length <= 10^4
+- -1000 <= nums[i] <= 1000
+"""
+
 from typing import List
 
 class Solution:
