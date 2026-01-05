043734ffab54264497a5917671d69bbfd3d5f658
bcmin1018 <bcmin1018@gmail.com>
2026-01-05 16:43:23 +0900

    add

---
+++ leetcode/graph/Number of Provinces.py
@@ -0,0 +1,40 @@
+from typing import List
+
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(0, size)]
+        self.rank = [1] * size
+        self.count = size
+    
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+    
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x != root_y:
+            if self.rank[root_x] > self.rank[root_y]:
+                self.root[root_y] = root_x
+            elif self.rank[root_x] < self.rank[root_y]:
+                self.root[root_x] = root_y
+            else:
+                self.root[root_y] = root_x
+            self.count -= 1
+            
+    def getCount(self):
+        return self.count
+    
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        if not isConnected or len(isConnected) == 0:
+            return 0
+        n = len(isConnected)
+        uf = UnionFind(n)
+        for row in range(n):
+            for col in range(row + 1, n):
+                if isConnected[row][col] == 1:
+                    uf.union(row, col)
+        return uf.getCount()
